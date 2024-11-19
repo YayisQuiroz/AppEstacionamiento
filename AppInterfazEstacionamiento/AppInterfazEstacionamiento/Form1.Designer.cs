f86e3b30060ad69be924a4735f7035adcbcdcf85
Yayis <126173181+YayisQuiroz@users.noreply.github.com>
2024-11-19 10:45:13 -0600

    Add files via upload

---
+++ AppInterfazEstacionamiento/AppInterfazEstacionamiento/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace AppInterfazEstacionamiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxRow1 = new System.Windows.Forms.GroupBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBoxRow2 = new System.Windows.Forms.GroupBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.groupBoxRow3 = new System.Windows.Forms.GroupBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.checkBox13 = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonStatus = new System.Windows.Forms.Button();
+            this.labelStatus1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxRow1.SuspendLayout();
+            this.groupBoxRow2.SuspendLayout();
+            this.groupBoxRow3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxRow1
+            // 
+            this.groupBoxRow1.Controls.Add(this.checkBox5);
+            this.groupBoxRow1.Controls.Add(this.checkBox4);
+            this.groupBoxRow1.Controls.Add(this.checkBox3);
+            this.groupBoxRow1.Controls.Add(this.checkBox2);
+            this.groupBoxRow1.Controls.Add(this.checkBox1);
+            this.groupBoxRow1.Location = new System.Drawing.Point(41, 24);
+            this.groupBoxRow1.Name = "groupBoxRow1";
+            this.groupBoxRow1.Size = new System.Drawing.Size(785, 128);
+            this.groupBoxRow1.TabIndex = 0;
+            this.groupBoxRow1.TabStop = false;
+            this.groupBoxRow1.Text = "Estacionamiento1";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(673, 53);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(86, 20);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Espacio5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(514, 53);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(86, 20);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Espacio4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(333, 53);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(86, 20);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Espacio3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(170, 53);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Espacio2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 53);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Espacio1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // groupBoxRow2
+            // 
+            this.groupBoxRow2.Controls.Add(this.checkBox6);
+            this.groupBoxRow2.Controls.Add(this.checkBox7);
+            this.groupBoxRow2.Controls.Add(this.checkBox8);
+            this.groupBoxRow2.Controls.Add(this.checkBox9);
+            this.groupBoxRow2.Controls.Add(this.checkBox10);
+            this.groupBoxRow2.Location = new System.Drawing.Point(41, 158);
+            this.groupBoxRow2.Name = "groupBoxRow2";
+            this.groupBoxRow2.Size = new System.Drawing.Size(785, 128);
+            this.groupBoxRow2.TabIndex = 5;
+            this.groupBoxRow2.TabStop = false;
+            this.groupBoxRow2.Text = "Estacionamiento2";
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(673, 53);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(93, 20);
+            this.checkBox6.TabIndex = 4;
+            this.checkBox6.Text = "Espacio10";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(514, 53);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(86, 20);
+            this.checkBox7.TabIndex = 3;
+            this.checkBox7.Text = "Espacio9";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(333, 53);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(86, 20);
+            this.checkBox8.TabIndex = 2;
+            this.checkBox8.Text = "Espacio8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(170, 53);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(86, 20);
+            this.checkBox9.TabIndex = 1;
+            this.checkBox9.Text = "Espacio7";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(23, 53);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(86, 20);
+            this.checkBox10.TabIndex = 0;
+            this.checkBox10.Text = "Espacio6";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // groupBoxRow3
+            // 
+            this.groupBoxRow3.Controls.Add(this.checkBox11);
+            this.groupBoxRow3.Controls.Add(this.checkBox12);
+            this.groupBoxRow3.Controls.Add(this.checkBox13);
+            this.groupBoxRow3.Controls.Add(this.checkBox14);
+            this.groupBoxRow3.Controls.Add(this.checkBox15);
+            this.groupBoxRow3.Location = new System.Drawing.Point(41, 305);
+            this.groupBoxRow3.Name = "groupBoxRow3";
+            this.groupBoxRow3.Size = new System.Drawing.Size(785, 128);
+            this.groupBoxRow3.TabIndex = 5;
+            this.groupBoxRow3.TabStop = false;
+            this.groupBoxRow3.Text = "Estacionamiento3";
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(673, 53);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(93, 20);
+            this.checkBox11.TabIndex = 4;
+            this.checkBox11.Text = "Espacio15";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            this.checkBox11.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(514, 53);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(93, 20);
+            this.checkBox12.TabIndex = 3;
+            this.checkBox12.Text = "Espacio14";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            this.checkBox12.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox13
+            // 
+            this.checkBox13.AutoSize = true;
+            this.checkBox13.Location = new System.Drawing.Point(333, 53);
+            this.checkBox13.Name = "checkBox13";
+            this.checkBox13.Size = new System.Drawing.Size(93, 20);
+            this.checkBox13.TabIndex = 2;
+            this.checkBox13.Text = "Espacio13";
+            this.checkBox13.UseVisualStyleBackColor = true;
+            this.checkBox13.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.Location = new System.Drawing.Point(170, 53);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(93, 20);
+            this.checkBox14.TabIndex = 1;
+            this.checkBox14.Text = "Espacio12";
+            this.checkBox14.UseVisualStyleBackColor = true;
+            this.checkBox14.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.AutoSize = true;
+            this.checkBox15.Location = new System.Drawing.Point(23, 53);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Size = new System.Drawing.Size(93, 20);
+            this.checkBox15.TabIndex = 0;
+            this.checkBox15.Text = "Espacio11";
+            this.checkBox15.UseVisualStyleBackColor = true;
+            this.checkBox15.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(919, 188);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(140, 35);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(919, 152);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(153, 30);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(847, 130);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(58, 22);
+            this.labelStatus.TabIndex = 10;
+            this.labelStatus.Text = "label1";
+            // 
+            // buttonStatus
+            // 
+            this.buttonStatus.Location = new System.Drawing.Point(919, 265);
+            this.buttonStatus.Name = "buttonStatus";
+            this.buttonStatus.Size = new System.Drawing.Size(53, 45);
+            this.buttonStatus.TabIndex = 11;
+            this.buttonStatus.UseVisualStyleBackColor = true;
+            // 
+            // labelStatus1
+            // 
+            this.labelStatus1.AutoSize = true;
+            this.labelStatus1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus1.Location = new System.Drawing.Point(847, 243);
+            this.labelStatus1.Name = "labelStatus1";
+            this.labelStatus1.Size = new System.Drawing.Size(53, 20);
+            this.labelStatus1.TabIndex = 12;
+            this.labelStatus1.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(846, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 29);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Controles de Estacionamiento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 469);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelStatus1);
+            this.Controls.Add(this.buttonStatus);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.groupBoxRow3);
+            this.Controls.Add(this.groupBoxRow2);
+            this.Controls.Add(this.groupBoxRow1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxRow1.ResumeLayout(false);
+            this.groupBoxRow1.PerformLayout();
+            this.groupBoxRow2.ResumeLayout(false);
+            this.groupBoxRow2.PerformLayout();
+            this.groupBoxRow3.ResumeLayout(false);
+            this.groupBoxRow3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxRow1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBoxRow2;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.GroupBox groupBoxRow3;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox checkBox13;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonStatus;
+        private System.Windows.Forms.Label labelStatus1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AppInterfazEstacionamiento/AppInterfazEstacionamiento/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using CheckBox = System.Windows.Forms.CheckBox;
+using GroupBox = System.Windows.Forms.GroupBox;
+
+
+namespace AppInterfazEstacionamiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var checkBox in groupBoxRow1.Controls.OfType<CheckBox>())
+                checkBox.CheckedChanged += CheckBox_CheckedChanged;
+
+            foreach (var checkBox in groupBoxRow2.Controls.OfType<CheckBox>())
+                checkBox.CheckedChanged += CheckBox_CheckedChanged;
+
+            foreach (var checkBox in groupBoxRow3.Controls.OfType<CheckBox>())
+                checkBox.CheckedChanged += CheckBox_CheckedChanged;
+            UpdateProgressBar();
+            UpdateLabel();
+        }
+        private void UpdateProgressBar()
+        {
+            int totalSpaces = 15;
+            int occupiedSpaces = groupBoxRow1.Controls.OfType<CheckBox>().Count(cb => cb.Checked) +
+                                 groupBoxRow2.Controls.OfType<CheckBox>().Count(cb => cb.Checked) +
+                                 groupBoxRow3.Controls.OfType<CheckBox>().Count(cb => cb.Checked);
+
+            progressBar1.Maximum = totalSpaces;
+            progressBar1.Value = occupiedSpaces;
+        }
+        private void UpdateLabel()
+        {
+            int totalSpaces = 15;
+            int occupiedSpaces = groupBoxRow1.Controls.OfType<CheckBox>().Count(cb => cb.Checked) +
+                                 groupBoxRow2.Controls.OfType<CheckBox>().Count(cb => cb.Checked) +
+                                 groupBoxRow3.Controls.OfType<CheckBox>().Count(cb => cb.Checked);
+
+            int availableSpaces = totalSpaces - occupiedSpaces;
+
+            labelStatus.Text = $"Espacios Ocupados: {occupiedSpaces} | Disponibles: {availableSpaces}";
+
+            if (occupiedSpaces == totalSpaces)
+            {
+                buttonStatus.BackColor = System.Drawing.Color.Red;
+
+                labelStatus1.Text = "Estacionamiento: LLENO";
+            }
+            else
+            {
+                buttonStatus.BackColor = System.Drawing.Color.Green;
+                labelStatus1.Text = "Estacionamiento: DISPONIBLE";
+            }
+        }
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateProgressBar();
+            UpdateLabel();
+        }
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            foreach (var checkBox in Controls.OfType<GroupBox>()
+                                              .SelectMany(gb => gb.Controls.OfType<CheckBox>()))
+            {
+                checkBox.Checked = false;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateProgressBar(); // Actualiza el progreso
+            UpdateLabel(); // Actualiza el texto del Label
+        }
+    }
+}
